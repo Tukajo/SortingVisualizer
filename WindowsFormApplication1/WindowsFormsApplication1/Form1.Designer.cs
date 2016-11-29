664cb30104452dd377ad9ba64232d083bd25b2db
Timothy Frisch <Tjfrisch@gmail.com>
2016-11-29 07:20:38 -0600

    init

---
+++ WindowsFormApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace SortingVisualizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.algorithmComboBox = new System.Windows.Forms.ComboBox();
+            this.algorithmMenuLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataTypeComboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // algorithmComboBox
+            // 
+            this.algorithmComboBox.FormattingEnabled = true;
+            this.algorithmComboBox.Location = new System.Drawing.Point(12, 25);
+            this.algorithmComboBox.Name = "algorithmComboBox";
+            this.algorithmComboBox.Size = new System.Drawing.Size(172, 21);
+            this.algorithmComboBox.TabIndex = 0;
+            this.algorithmComboBox.Text = "-- Select Item --";
+            this.algorithmComboBox.SelectedIndexChanged += new System.EventHandler(this.algorithm_Menu_SelectIndexChanged);
+            // 
+            // algorithmMenuLabel
+            // 
+            this.algorithmMenuLabel.AutoSize = true;
+            this.algorithmMenuLabel.Location = new System.Drawing.Point(12, 9);
+            this.algorithmMenuLabel.Name = "algorithmMenuLabel";
+            this.algorithmMenuLabel.Size = new System.Drawing.Size(83, 13);
+            this.algorithmMenuLabel.TabIndex = 1;
+            this.algorithmMenuLabel.Text = "Select Algorithm";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(407, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 508);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(404, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Psuedocode";
+            // 
+            // dataTypeComboBox1
+            // 
+            this.dataTypeComboBox1.FormattingEnabled = true;
+            this.dataTypeComboBox1.Location = new System.Drawing.Point(12, 76);
+            this.dataTypeComboBox1.Name = "dataTypeComboBox1";
+            this.dataTypeComboBox1.Size = new System.Drawing.Size(121, 21);
+            this.dataTypeComboBox1.TabIndex = 4;
+            this.dataTypeComboBox1.Text = "-- Select Data --";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Select Data";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(245, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Statistics";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 558);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataTypeComboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.algorithmMenuLabel);
+            this.Controls.Add(this.algorithmComboBox);
+            this.Name = "Form1";
+            this.Text = "Sorting Visualizer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox algorithmComboBox;
+        private System.Windows.Forms.Label algorithmMenuLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox dataTypeComboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortingVisualizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            algorithmComboBox.Items.Add("MergeSort");
+            algorithmComboBox.Items.Add("QuickSort");
+            algorithmComboBox.Items.Add("BubbleSort");
+            dataTypeComboBox1.Items.Add("String, 100");
+            dataTypeComboBox1.Items.Add("String, 1,000");
+            dataTypeComboBox1.Items.Add("String, 10,000");
+            dataTypeComboBox1.Items.Add("Integer, 100");
+            dataTypeComboBox1.Items.Add("Integer, 1,000");
+            dataTypeComboBox1.Items.Add("Integer, 10,000");
+        }
+
+        private void algorithm_Menu_SelectIndexChanged(object Sender, EventArgs e)
+        {
+       
+            
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
